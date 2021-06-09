2805b199849bbeb51eb6b71296a4e36dd4ba7a6f
heesun <yh82076@naver.com>
2021-06-09 23:36:35 +0900

    Toy_Project_ver 1.49

--- 05_Develop/Spring/ScheduleProject/src/main/java/com/project/test/DBTest.java
+++ 05_Develop/Spring/ScheduleProject/src/main/java/com/project/test/DBTest.java
@@ -8,16 +8,18 @@ import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/root-context.xml" })
 public class DBTest {
 	
     @Inject
-    private DataSource ds;
+//    private DataSource ds;
+    private SqlSessionTemplate ds;
+    
  
     @Test
     public void testConnection(){
