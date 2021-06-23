f1a42985ca74f11100094951f23d11e6ffa28bcd
heesun <yh82076@naver.com>
2021-06-23 23:39:25 +0900

    Toy_Project_ver 1.55

---
+++ 05_Develop/Spring/ScheduleProject/src/main/java/com/project/common/Main.java
@@ -0,0 +1,17 @@
+package com.project.common;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class Main {
+	@RequestMapping(value="/main.do")
+	public String mainPage (HttpServletRequest request, HttpServletRequest response) {
+		return "/main";
+	}
+}
