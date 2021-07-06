6c247a592e085c61cac895b4b4c4d69df6eb6203
heesun <yh82076@naver.com>
2021-07-06 23:04:42 +0900

    Toy_Project_ver 1.81

--- 05_Develop/Spring/ScheduleProject/src/main/java/com/project/common/LoginController.java
+++ 05_Develop/Spring/ScheduleProject/src/main/java/com/project/common/LoginController.java
@@ -1,6 +1,7 @@
 package com.project.common;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,18 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class LoginController {
 	@RequestMapping(value="/login.do", method = {RequestMethod.POST, RequestMethod.GET} )
-	public String loginPage (HttpServletRequest request, HttpServletRequest response) {
+	public String loginPage (MemberDTO dto, HttpServletRequest request, HttpServletRequest response) {
+		
+		//로그인 세션
+		HttpSession session = request.getSession();
+		
+		//세션 유지시간 설정 (30분)
+		session.setMaxInactiveInterval(1800);
+		
+		session.setAttribute("seq", dto.getSeq());
+		session.setAttribute("id", dto.getSeq());
+		session.setAttribute("pw", dto.getPw());
+		
 		return "/common/login";
 	}
 
