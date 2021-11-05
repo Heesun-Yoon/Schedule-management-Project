f176ee124351974ec64a3846226235b6ce529a5b
heesun <yh82076@naver.com>
2021-11-06 00:12:37 +0900

    Toy_Project_ver 4.44

--- 04_DB/Oracle/toy_ddl.sql
+++ 04_DB/Oracle/toy_ddl.sql
@@ -115,7 +115,7 @@ select seq, member_seq, title, content, start_time, end_time, important, tag, co
         WHEN start_time < to_char(SYSDATE,'yyyy.mm.dd') AND end_time > to_char(SYSDATE,'yyyy.mm.dd') THEN 'DOING'
         WHEN end_time < to_char(SYSDATE,'yyyy.mm.dd') THEN 'DONE'
         END as state        
-from tbl_schedule where member_seq = (select seq from tbl_member where id='HeeSun' and pw='1234');
+from tbl_schedule where member_seq = (select seq from tbl_member where id='HeeSun' and pw='1234') and tag LIKE '%coding%';
 
 
 
